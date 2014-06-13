a7c30b18b1563634e94ead30243bdbddf411b00d
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2014-06-13 16:09:23 +0200

    Adding Groovy and Spock for testing

---
+++ core/src/test/groovy/com/odobo/springsecurity/rest/core/RestAuthenticationFilterSpec.groovy
@@ -0,0 +1,16 @@
+package com.odobo.springsecurity.rest.core
+
+import spock.lang.Specification
+
+
+/**
+ * Created by mariscal on 13/06/14.
+ */
+class RestAuthenticationFilterSpec extends Specification {
+
+    void "it works"(){
+        expect:
+        true
+    }
+
+}
